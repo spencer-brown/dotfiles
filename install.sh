92f10573bd9a13ddf920f84e9f1249bdc91bff4b
Spencer Brown <spencer@mixmax.com>
2017-03-06 16:20:24 -0800

    Add working install script

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-# Creates symlinks for these configuration files.
+# Creates symlinks for these configuration files. You will be prompted to confirm the replacement of
+# any existing configuration files before they are replaced.
 
-ln -s .vimrc $HOME/.foorc
+PWD=$(pwd)
+
+ln -si $PWD/.vimrc $HOME/.vimrc
+ln -si $PWD/.tmux.conf $HOME/.tmux.conf
+ln -si $PWD/.zshrc $HOME/.zshrc
+ln -si $PWD/spencer.zsh-theme $HOME/.oh-my-zsh/themes/spencer.zsh-theme
